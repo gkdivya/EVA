e86296fdc263f5a4dd4a811c710aab5175470c3d
Divya Kamat <42609155+divya-r-kamat@users.noreply.github.com>
2021-06-06 18:29:28 +0530

    Create test.py

---
+++ 6_BatchNormalization_Regularization/utils/test.py
@@ -0,0 +1,26 @@
+import torch
+import torch.nn.functional as F
+
+
+def test(model, device, test_loader):
+    model.eval()
+    test_losses = []
+    test_acc = []
+    test_loss = 0
+    correct = 0
+    with torch.no_grad():
+        for data, target in test_loader:
+            data, target = data.to(device), target.to(device)
+            output = model(data)
+            test_loss += F.nll_loss(output, target, reduction='sum').item()  # sum up batch loss
+            pred = output.argmax(dim=1, keepdim=True)  # get the index of the max log-probability
+            correct += pred.eq(target.view_as(pred)).sum().item()
+
+    test_loss /= len(test_loader.dataset)
+    test_losses.append(test_loss)
+
+    print('\nTest set: Average loss: {:.4f}, Accuracy: {}/{} ({:.2f}%)\n'.format(
+        test_loss, correct, len(test_loader.dataset),
+        100. * correct / len(test_loader.dataset)))
+    
+    test_acc.append(100. * correct / len(test_loader.dataset))
