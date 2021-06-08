579244458c498669b7e107197db02ce58fda911e
Divya Kamat <42609155+divya-r-kamat@users.noreply.github.com>
2021-06-08 19:23:17 +0530

    Create plot_graphs.py

---
+++ 6_BatchNormalization_Regularization/utils/plot_graphs.py
@@ -0,0 +1,32 @@
+%matplotlib inline
+import matplotlib.pyplot as plt
+
+
+def plot_metrics(results):
+  fig, axs = plt.subplots(2,2,figsize=(15,10))
+  for idx, exp_name in enumerate(results.keys()):
+      train_accuracy,train_losses,test_accuracy,test_losses  = results[exp_name]
+      axs[0, 0].set_title("Training Loss")
+      axs[1, 0].set_title("Training Accuracy")
+      axs[0, 1].set_title("Test Loss")
+      axs[1, 1].set_title("Test Accuracy")
+
+      axs[0, 0].plot(train_losses, label='{} reg'.format(exp_name))
+      axs[0,0].legend(loc='upper right')
+      axs[0,0].set_xlabel('epochs')
+      axs[0,0].set_ylabel('loss')
+
+      axs[1, 0].plot(train_accuracy, label='{} reg'.format(exp_name))
+      axs[1,0].legend(loc='upper right')
+      axs[1,0].set_xlabel('epochs')
+      axs[1,0].set_ylabel('loss')
+
+      axs[0, 1].plot(test_losses, label='{} reg'.format(exp_name))
+      axs[0,1].legend(loc='upper right')
+      axs[0,1].set_xlabel('epochs')
+      axs[0,1].set_ylabel('loss')
+
+      axs[1, 1].plot(test_accuracy, label='{} reg'.format(exp_name))
+      axs[1,1].legend(loc='upper right')
+      axs[1,1].set_xlabel('epochs')
+      axs[1,1].set_ylabel('loss')
