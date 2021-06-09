c3945c05cf46992b41e3e8fb7d13bdb14388a82c
Divya Kamat <42609155+divya-r-kamat@users.noreply.github.com>
2021-06-09 16:49:30 +0530

    Update helper.py

--- 6_BatchNormalization_Regularization/utils/helper.py
+++ 6_BatchNormalization_Regularization/utils/helper.py
@@ -2,6 +2,15 @@ import torch
 import torch.optim as optim
 import matplotlib.pyplot as plt
 from torch.optim.lr_scheduler import StepLR,OneCycleLR
+from utils import model as m
+from utils import train as trn
+from utils import test as tst
+
+
+def model_summary(model, input_size):
+    result = summary(model, input_size=input_size)
+    print(result)
+    
 
 def experiments(train_loader, test_loader, norm_type, l1_factor, l2_factor, dropout, epochs):
     
@@ -13,18 +22,19 @@ def experiments(train_loader, test_loader, norm_type, l1_factor, l2_factor, drop
     use_cuda = torch.cuda.is_available()
     device = torch.device("cuda" if use_cuda else "cpu")
 
-    model = Net(norm_type, dropout).to(device)
+    model = m.Net(norm_type, dropout).to(device)
     optimizer = optim.SGD(model.parameters(), lr=0.015, momentum=0.7)
     scheduler = OneCycleLR(optimizer, max_lr=0.015,epochs=epochs,steps_per_epoch=len(train_loader))
     epochs = epochs
 
     for epoch in range(1, epochs + 1):
         print(f'Epoch {epoch}:')
-        train(model, device, train_loader, optimizer, epoch, train_accuracy, train_losses, l1_factor,scheduler)
-        test(model, device, test_loader,test_accuracy,test_losses)
+        trn.train(model, device, train_loader, optimizer, epoch, train_accuracy, train_losses, l1_factor,scheduler)
+        tst.test(model, device, test_loader,test_accuracy,test_losses)
 
-    return train_accuracy,train_losses,test_accuracy,test_losses
-
+    return (train_accuracy,train_losses,test_accuracy,test_losses),model
+    
+    
 def wrong_predictions(test_loader,model,device):
   wrong_images=[]
   wrong_label=[]
@@ -41,18 +51,19 @@ def wrong_predictions(test_loader,model,device):
       correct_label.append(target.view_as(pred)[wrong_pred])  
       
       wrong_predictions = list(zip(torch.cat(wrong_images),torch.cat(wrong_label),torch.cat(correct_label)))    
-      print(f'Total wrong predictions are {len(wrong_predictions)}')
+    print(f'Total wrong predictions are {len(wrong_predictions)}')
       
       
-      fig = plt.figure(figsize=(20,25))
-      for i, (img, pred, correct) in enumerate(wrong_predictions[:20]):
+    fig = plt.figure(figsize=(8,10))
+    fig.tight_layout()
+    for i, (img, pred, correct) in enumerate(wrong_predictions[:10]):
           img, pred, target = img.cpu().numpy(), pred.cpu(), correct.cpu()
-          ax = fig.add_subplot(10, 10, i+1)
+          ax = fig.add_subplot(5, 2, i+1)
           ax.axis('off')
-          ax.set_title(f'actual {target.item()}\npredicted {pred.item()}',fontsize=15)
-          ax.imshow(img.squeeze())
-      plt.show()
+          ax.set_title(f'\nactual {target.item()}\npredicted {pred.item()}',fontsize=10)  
+          ax.imshow(img.squeeze(), cmap='gray_r')  
+          
+    plt.show()
       
   return 
-
-
+  
