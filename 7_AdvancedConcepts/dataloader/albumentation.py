d7afa78219a32e2f7ea892ec617e6a727bc9aba4
Garvit Garg <garvit.gargs@gmail.com>
2021-06-19 01:05:30 +0530

    Update albumentation.py

--- 7_AdvancedConcepts/dataloader/albumentation.py
+++ 7_AdvancedConcepts/dataloader/albumentation.py
@@ -1,1 +1,26 @@
+import albumentations as A
+from albumentations.pytorch.transforms import ToTensorV2
+import numpy as np
+from utils import helper
 
+def data_albumentations(horizontalflip_prob,rotate_limit,shiftscalerotate_prob,num_holes,cutout_prob):
+    # Calculate mean and std deviation for cifar dataset
+    mean,std = helper.calculate_mean_std("CIFAR10")
+    
+    # Train Phase transformations
+    train_transforms = A.Compose([A.HorizontalFlip(p=horizontalflip_prob),
+                                  A.ShiftScaleRotate(shift_limit=0.1, scale_limit=0.1, rotate_limit=rotate_limit, p=shiftscalerotate_prob),
+                                  A.CoarseDropout(max_holes=num_holes,min_holes = 1, max_height=16, max_width=16, 
+                                  p=cutout_prob,fill_value=tuple([x * 255.0 for x in mean]),
+                                  min_height=16, min_width=16),
+                                  A.ColorJitter(p=0.25,brightness=0.3, contrast=0.3, saturation=0.30, hue=0.2),
+                                  A.ToGray(p=0.15),
+                                  A.Normalize(mean=mean, std=std,always_apply=True),
+                                  ToTensorV2()
+                                ])
+
+    # Test Phase transformations
+    test_transforms = A.Compose([A.Normalize(mean=mean, std=std, always_apply=True),
+                                 ToTensorV2()])
+
+    return lambda img:train_transforms(image=np.array(img))["image"],lambda img:test_transforms(image=np.array(img))["image"]
